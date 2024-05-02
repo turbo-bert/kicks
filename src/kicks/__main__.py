608034df1edf4afb22be36222214d9bb2d58d31d
Robert Degen <robert.degen@mailbox.org>
2024-05-03 01:40:48 +0200

    ...

--- src/kicks/__main__.py
+++ src/kicks/__main__.py
@@ -9,6 +9,14 @@ from rich.console import Console
 from rich.table import Table
 CONSOLE = Console()
 
+
+# curl -o p.exe -L https://www.python.org/ftp/python/3.12.3/python-3.12.3-amd64.exe
+# p.exe PrependPath=1 /passive
+# cmd reopen
+# pip install kicks
+# python -m kicks
+
+
 # written by robert degen
 # 2015-2024
 # notes:
@@ -26,6 +34,7 @@ home = os.getenv("USERPROFILE")
 OP=os.path.join(home, "_kicks")
 OD=os.makedirs(OP, exist_ok=True)
 
+
 def tryversion(pkg):
     import importlib.metadata
     try:
@@ -34,8 +43,13 @@ def tryversion(pkg):
         res = pkg + '-dev'
     return res
 
+
 def generic_pip_install(pkg_name, rcon):
-    pass
+    install_command = 'pip install "%s"' % pkg_name
+    confirm = CONSOLE.input("run '%s' ? # 'y' to continue... " % install_command)
+    if confirm == "y":
+        subprocess.call(install_command, shell=True)
+
 
 t = Table(title=tryversion("kicks"))
 t.add_column("#")
@@ -52,12 +66,12 @@ t.add_section()
 t.add_row("4", "process explorer", "wp", "latest wordpress")
 t.add_row("5", "vc_redist", "www", "WP->htdocs (req:wp)")
 t.add_row("7", "grml small iso", "2k22", "windows server 2022 en iso")
-t.add_row("8", "fpc win64")
+t.add_row("8", "fpc win64", "j", "jenkins")
 t.add_row("9", "sublime", "npp", "notepad plus plus")
-t.add_row("10", "emacs")
-t.add_row("11", "cygwin")
+t.add_row("10", "emacs", "j21", "Java JDK v21 Oracle")
+t.add_row("11", "cygwin", "j22", "Java JDK v22 Oracle")
 t.add_row("12", "git", "is", "inno setup")
-t.add_row("13", "xampp")
+t.add_row("13", "xampp", "sshd1-6", "Set Up SSHd with key auth")
 t.add_row("14", "visual studio code", "15", "LibreOffice")
 t.add_section()
 t.add_row("q", "QUIT")
@@ -119,15 +133,19 @@ while True:
         
         #dcx / python: daisy chain xpath
         with open(os.path.join(bindir, 'dcx.bat'), 'w') as f:
-            f.write('python -m dcx %%*')
+            f.write('python -m dcx %*')
+
+        #eppu
+        with open(os.path.join(bindir, 'eppu.bat'), 'w') as f:
+            f.write('python -m eppu %*')
 
         #dcxc / python: daisy chain xpath CHROME
         with open(os.path.join(bindir, 'dcxc.bat'), 'w') as f:
-            f.write('python -m dcx --local-chrome %%*')
+            f.write('python -m dcx --local-chrome %*')
 
         #dcxe / python: daisy chain xpath EDGE
         with open(os.path.join(bindir, 'dcxe.bat'), 'w') as f:
-            f.write('python -m dcx --local-edge %%*')
+            f.write('python -m dcx --local-edge %*')
 
         #kicks / python: kickstarter for windows64
         with open(os.path.join(bindir, 'kicks.bat'), 'w') as f:
@@ -163,7 +181,7 @@ while True:
         subprocess.call(cmd, shell=True)
         run_confirm = CONSOLE.input('run it? type "run" to execute or anything else to exit # ')
         if run_confirm == "run":
-            subprocess.call(outfile, shell=True)
+            subprocess.call("%s /S" % outfile, shell=True)
 
     if x == "2":
         outfile = os.path.join(OP, "chrome.zip")
@@ -175,6 +193,31 @@ while True:
             subprocess.call("""powershell -Command Expand-Archive -Path '%s' -DestinationPath '%s'""" % (outfile, outzipdir), shell=True)
             subprocess.call("""%s""" % os.path.join(outzipdir, "Installers", "GoogleChromeStandaloneEnterprise64.msi"), shell=True)
             #subprocess.call("""explorer %s""" % (outzipdir), shell=True)
+
+    if x == "j":
+        #https://www.jenkins.io/download/
+        outfile = os.path.join(OP, "jenkins.msi")
+        cmd = 'curl -C - -L -o "%s" "https://get.jenkins.io/windows-stable/2.440.3/jenkins.msi"' % outfile
+        subprocess.call(cmd, shell=True)
+        run_confirm = CONSOLE.input('run it? type "run" to execute or anything else to exit # ')
+        if run_confirm == "run":
+            subprocess.call("""msiexec.exe /i %s /passive JENKINSDIR="C:\\Program Files\\Jenkins\\" PORTNUMBER=18080 JENKINS_ROOT="%%ProgramData%%\\Jenkins\\" JAVA_HOME="C:\\Program Files\\Java\\jdk-21\\" SERVICE_LOGON_TYPE=ServiceLocalSystem ALLUSERS=1""" % outfile, shell=True)
+
+    if x == "j21":
+        outfile = os.path.join(OP, "j21.msi")
+        cmd = 'curl -C - -L -o "%s" "https://download.oracle.com/java/21/latest/jdk-21_windows-x64_bin.msi"' % outfile
+        subprocess.call(cmd, shell=True)
+        run_confirm = CONSOLE.input('run it? type "run" to execute or anything else to exit # ')
+        if run_confirm == "run":
+            subprocess.call("msiexec.exe /i %s /qn" % outfile, shell=True)
+
+    if x == "j22":
+        outfile = os.path.join(OP, "j22.msi")
+        cmd = 'curl -C - -L -o "%s" "https://download.oracle.com/java/22/latest/jdk-22_windows-x64_bin.msi"' % outfile
+        subprocess.call(cmd, shell=True)
+        run_confirm = CONSOLE.input('run it? type "run" to execute or anything else to exit # ')
+        if run_confirm == "run":
+            subprocess.call("msiexec.exe /i %s /qn" % outfile, shell=True)
 
     if x == "wp":
         outfile = os.path.join(OP, "wp.zip")
@@ -218,7 +261,7 @@ while True:
         subprocess.call(cmd, shell=True)
         run_confirm = CONSOLE.input('run it? type "run" to execute or anything else to exit # ')
         if run_confirm == "run":
-            subprocess.call(outfile, shell=True)
+            subprocess.call("%s /install /passive" % outfile, shell=True)
 
     if x == "6":
         #https://www.virtualbox.org/wiki/Downloads
@@ -328,6 +371,40 @@ while True:
         run_confirm = CONSOLE.input('run it? type "run" to execute or anything else to exit # ')
         if run_confirm == "run":
             subprocess.call("%s /passive /qr" % outfile, shell=True)
+
+    if x == "sshd1":
+        #https://learn.microsoft.com/en-us/windows-server/administration/openssh/openssh_install_firstuse?tabs=powershell
+        cmd="""powershell -Command Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0"""
+        subprocess.call(cmd, shell=True)
+        cmd="""powershell -Command Start-Service sshd"""
+        subprocess.call(cmd, shell=True)
+        cmd="""powershell -Command Stop-Service sshd"""
+        subprocess.call(cmd, shell=True)
+
+    if x == "sshd2":
+        #https://learn.microsoft.com/en-us/windows-server/administration/openssh/openssh_install_firstuse?tabs=powershell
+        cmd="""notepad C:\\ProgramData\\ssh\\administrators_authorized_keys"""
+        subprocess.call(cmd, shell=True)
+
+    if x == "sshd3":
+        #https://learn.microsoft.com/en-us/windows-server/administration/openssh/openssh_install_firstuse?tabs=powershell
+        cmd="""move C:\\ProgramData\\ssh\\administrators_authorized_keys.txt C:\\ProgramData\\ssh\\administrators_authorized_keys"""
+        subprocess.call(cmd, shell=True)
+
+    if x == "sshd4":
+        #https://learn.microsoft.com/en-us/windows-server/administration/openssh/openssh_install_firstuse?tabs=powershell
+        cmd="""icacls.exe "C:\\ProgramData\\ssh\\administrators_authorized_keys" /inheritance:r /grant "Administrators:F" /grant "SYSTEM:F" """
+        subprocess.call(cmd, shell=True)
+
+    if x == "sshd5":
+        #https://learn.microsoft.com/en-us/windows-server/administration/openssh/openssh_install_firstuse?tabs=powershell
+        cmd="""powershell -Command Start-Service sshd"""
+        subprocess.call(cmd, shell=True)
+
+    if x == "sshd6":
+        #https://learn.microsoft.com/en-us/windows-server/administration/openssh/openssh_install_firstuse?tabs=powershell
+        cmd="""powershell -Command Set-Service -Name sshd -StartupType 'Automatic'"""
+        subprocess.call(cmd, shell=True)
 
     if x == "q":
         break
